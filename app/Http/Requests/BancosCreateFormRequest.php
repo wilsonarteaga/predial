4976ddc9d329732f85fc9d3fbffd80067241944c
Wilson Arteaga <wilson.arteaga@gmail.com>
2022-03-14 18:24:47 -0500

    Desarrollo Bancos

--- app/Http/Requests/BancosCreateFormRequest.php
+++ app/Http/Requests/BancosCreateFormRequest.php
@@ -13,7 +13,7 @@ class BancosCreateFormRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,24 @@ class BancosCreateFormRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'codigo'  => 'required|max:10',
+            'nombre'  => 'required|max:128',
+            'cuenta_contable'  => 'required|max:30',
+            'asobancaria'  => 'required|max:30'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'codigo.required' => 'El campo código es obligatorio.',
+            'nombre.required' => 'El campo nombre es obligatorio.',
+            'codigo.max' => 'Máximo :max caracteres.',
+            'nombre.max' => 'Máximo :max caracteres.',
+            'cuenta_contable.required'  => 'El campo cuenta contable es obligatorio.',
+            'asobancaria.required'  => 'El campo asobancaria es obligatorio.',
+            'cuenta_contable.max' => 'Máximo :max caracteres.',
+            'asobancaria.max' => 'Máximo :max caracteres.',
         ];
     }
 }
--- app/Http/Requests/BancosUpdateFormRequest.php
+++ app/Http/Requests/BancosUpdateFormRequest.php
@@ -13,7 +13,7 @@ class BancosUpdateFormRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,24 @@ class BancosUpdateFormRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            //'codigo'  => 'required|max:10',
+            'nombre_edit'  => 'required|max:128',
+            'cuenta_contable_edit'  => 'required|max:30',
+            'asobancaria_edit'  => 'required|max:30'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            //'codigo.required' => 'El campo código es obligatorio.',
+            'nombre_edit.required' => 'El campo nombre es obligatorio.',
+            'codigo_edit.max' => 'Máximo :max caracteres.',
+            'nombre_edit.max' => 'Máximo :max caracteres.',
+            'cuenta_contable_edit.required'  => 'El campo cuenta contable es obligatorio.',
+            'asobancaria_edit.required'  => 'El campo asobancaria es obligatorio.',
+            'cuenta_contable_edit.max' => 'Máximo :max caracteres.',
+            'asobancaria_edit.max' => 'Máximo :max caracteres.',
         ];
     }
 }
