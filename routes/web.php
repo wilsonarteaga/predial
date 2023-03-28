3f39d8b0fbb58be62d74f3008ea47c51cbbecc39
Wilson Arteaga <wilson.arteaga@gmail.com>
2023-03-28 12:23:03 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AdministracionController;
 use App\Http\Controllers\AniosController;
 use App\Http\Controllers\BancosController;
 use App\Http\Controllers\ChangePasswordController;
@@ -14,6 +15,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PagosController;
 use App\Http\Controllers\PrediosController;
 use App\Http\Controllers\TarifasPredialController;
+use App\Http\Controllers\TasasInteresController;
 use App\Http\Controllers\TiposPredioController;
 use App\Http\Controllers\ResolucionesIgacController;
 use App\Http\Controllers\PrediosExoneracionesController;
@@ -43,106 +45,13 @@ Route::get('profile', [LoginController::class, 'profile'])->middleware('checkdb'
 Route::get('cambiar_password', [ChangePasswordController::class, 'index'])->name('auth.index_change_pass')->middleware('checkdb');
 Route::post('changepass', [ChangePasswordController::class, 'changepass'])->name('auth.change_pass')->middleware('checkdb');
 
-//Pacientes y Acudientes
-// Route::get('atencion_usuarios', [AtencionUsuariosController::class, 'atencion_usuarios'])->middleware('checkdb');
-
-// Route::get('registro_pacientes/{id}', [AtencionUsuariosController::class, 'registro_pacientes'])->middleware('checkdb');
-// Route::get('registro_pacientes', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('reporte_pacientes_au/{id}', [AtencionUsuariosController::class, 'reporte_pacientes'])->middleware('checkdb');
-// Route::get('reporte_pacientes_au', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('registro_acudientes/{id}', [AtencionUsuariosController::class, 'registro_acudientes'])->middleware('checkdb');
-// Route::get('registro_acudientes', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('acudientes_paciente/{id}', [AtencionUsuariosController::class, 'acudientes_paciente'])->middleware('checkdb');
-// Route::get('acudientes_paciente', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::post('createpac', [AtencionUsuariosController::class, 'createpac'])->name('aten.create_pac')->middleware('checkdb');
-// Route::post('updatepac', [AtencionUsuariosController::class, 'updatepac'])->name('aten.update_pac')->middleware('checkdb');
-// Route::post('deletepac', [AtencionUsuariosController::class, 'deletepac'])->name('aten.delete_pac')->middleware('checkdb');
-
-// Route::post('createasoc', [AtencionUsuariosController::class, 'createasoc'])->name('aten.create_asoc')->middleware('checkdb');
-// Route::post('updateasoc', [AtencionUsuariosController::class, 'updateasoc'])->name('aten.update_asoc')->middleware('checkdb');
-// Route::post('deleteasoc', [AtencionUsuariosController::class, 'deleteasoc'])->name('aten.delete_asoc')->middleware('checkdb');
-
-// Route::post('createacu', [AtencionUsuariosController::class, 'createacu'])->name('aten.create_acu')->middleware('checkdb');
-// Route::post('updateacu', [AtencionUsuariosController::class, 'updateacu'])->name('aten.update_acu')->middleware('checkdb');
-// Route::post('deleteacu', [AtencionUsuariosController::class, 'deleteacu'])->name('aten.delete_acu')->middleware('checkdb');
-
-// Route::get('citas/{id}', [AtencionUsuariosController::class, 'citas'])->middleware('checkdb');
-// Route::get('citas_paciente/{id}/{paciente}', [AtencionUsuariosController::class, 'citas_paciente'])->middleware('checkdb');
-// Route::get('citas', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-// Route::get('citas_paciente', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-// Route::get('citas_paciente/{id}', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::post('createcit', [AtencionUsuariosController::class, 'createcit'])->name('aten.create_cit')->middleware('checkdb');
-// Route::post('updatecit', [AtencionUsuariosController::class, 'updatecit'])->name('aten.update_cit')->middleware('checkdb');
-
-// Route::get('/available/hours', [AtencionUsuariosController::class, 'getAvailableHours']);
-// Route::get('/iavailable/hours', [AtencionUsuariosController::class, 'getAvailableHoursInclusive']);
-
-//Fonoaudiologia
-// Route::get('dislex_game/{id}/{id_cita}', [FonoaudiologiaController::class, 'dislex_game'])->middleware('checkdb');
-// Route::get('dislex_game', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-// Route::get('dislex_game/{id}', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('historial_medico/{id}', [FonoaudiologiaController::class, 'historial_medico'])->middleware('checkdb');
-// Route::get('historial_medico', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('remision/{id}', [FonoaudiologiaController::class, 'remision'])->middleware('checkdb');
-// Route::get('remision', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('reporte_pacientes_fo/{id}', [FonoaudiologiaController::class, 'reporte_pacientes'])->middleware('checkdb');
-// Route::get('reporte_pacientes_fo', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('citas_fonoaudiologo/{id}', [FonoaudiologiaController::class, 'citas_fonoaudiologo'])->middleware('checkdb');
-// Route::get('citas_fonoaudiologo', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::post('createhist', [FonoaudiologiaController::class, 'createhist'])->name('fono.create_hist')->middleware('checkdb');
-// Route::post('updatehist', [FonoaudiologiaController::class, 'updatehist'])->name('fono.update_hist')->middleware('checkdb');
-
-// Route::post('createrem', [FonoaudiologiaController::class, 'createrem'])->name('fono.create_rem')->middleware('checkdb');
-// Route::post('updaterem', [FonoaudiologiaController::class, 'updaterem'])->name('fono.update_rem')->middleware('checkdb');
-// Route::post('deleterem', [FonoaudiologiaController::class, 'deleterem'])->name('fono.delete_rem')->middleware('checkdb');
-
 //Administracion
-// Route::get('registro_fonoaudiologos/{id}', [AdministracionController::class, 'registro_fonoaudiologos'])->middleware('checkdb');
-// Route::get('registro_fonoaudiologos', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
+Route::get('registro_usuarios/{id}', [AdministracionController::class, 'registro_usuarios'])->middleware('checkdb');
+Route::get('registro_usuarios', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
 
-// Route::get('rubrica/{id}', [AdministracionController::class, 'rubrica'])->middleware('checkdb');
-// Route::get('rubrica', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('registro_usuarios/{id}', [AdministracionController::class, 'registro_usuarios'])->middleware('checkdb');
-// Route::get('registro_usuarios', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::get('registro_juegos/{id}', [AdministracionController::class, 'registro_juegos'])->middleware('checkdb');
-// Route::get('registro_juegos', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
-
-// Route::post('createfon', [AdministracionController::class, 'createfon'])->name('admin.create_fon')->middleware('checkdb');
-// Route::post('updatefon', [AdministracionController::class, 'updatefon'])->name('admin.update_fon')->middleware('checkdb');
-
-// Route::post('createrub', [AdministracionController::class, 'createrub'])->name('admin.create_rub')->middleware('checkdb');
-// Route::post('updaterub', [AdministracionController::class, 'updaterub'])->name('admin.update_rub')->middleware('checkdb');
-// Route::post('deleterub', [AdministracionController::class, 'deleterub'])->name('admin.delete_rub')->middleware('checkdb');
-
-// Route::post('createusu', [AdministracionController::class, 'createusu'])->name('admin.create_usu')->middleware('checkdb');
-// Route::post('updateusu', [AdministracionController::class, 'updateusu'])->name('admin.update_usu')->middleware('checkdb');
-// Route::post('deleteusu', [AdministracionController::class, 'deleteusu'])->name('admin.delete_usu')->middleware('checkdb');
-
-// Route::post('createjue', [AdministracionController::class, 'createjue'])->name('admin.create_jue')->middleware('checkdb');
-// Route::post('updatejue', [AdministracionController::class, 'updatejue'])->name('admin.update_jue')->middleware('checkdb');
-// Route::post('deletejue', [AdministracionController::class, 'deletejue'])->name('admin.delete_jue')->middleware('checkdb');
-
-// Route::get('generate-pdf', [AtencionUsuariosController::class, 'generatePDF'])->name('aten.patientreport')->middleware('checkdb');
-// Route::get('generate-pdf/{id}', [AtencionUsuariosController::class, 'generatePDFByIdPaciente'])->name('aten.singlepatientreport')->middleware('checkdb');
-
-//juego paciente
-// Route::get('dislex_game/gamedata', [FonoaudiologiaController::class, 'Data']);
-
-// Route::get('games', [JuegoPaciente::class, 'guardar']);
-
-// Route::get('jueguito', [JuegoPaciente::class, 'juego']);
+Route::post('createusu', [AdministracionController::class, 'createusu'])->name('admin.create_usu')->middleware('checkdb');
+Route::post('updateusu', [AdministracionController::class, 'updateusu'])->name('admin.update_usu')->middleware('checkdb');
+Route::post('deleteusu', [AdministracionController::class, 'deleteusu'])->name('admin.delete_usu')->middleware('checkdb');
 
 //AniosController
 Route::get('registro_anios/{id}', [AniosController::class, 'create'])->middleware('checkdb');
@@ -215,15 +124,21 @@ Route::post('prescribe_predios', [PrediosController::class, 'prescribe'])->name(
 
 Route::get('/show/predios_datos', [PrediosController::class, 'show_predios_datos']);
 Route::get('generate_factura_pdf/{id}/{tmp}/{anios}', [PrediosController::class, 'generate_factura_pdf'])->middleware('checkdb');
+// Route::get('generate_factura_pdf/{id}/{vistaPrevia}/{anio}/{cuotas}/{fecha_pago}', [PrediosController::class, 'generate_factura_pdf'])->middleware('checkdb');
 Route::get('generate_paz_pdf/{id}/{destino}/{fecha}/{valor}', [PrediosController::class, 'generate_paz_pdf'])->middleware('checkdb');
+Route::get('generate_avaluos_predio_pdf/{id}', [PrediosController::class, 'generate_avaluos_predio_pdf'])->middleware('checkdb');
 Route::get('/autocomplete', [PrediosController::class, 'autocomplete']);
+Route::get('/autocomplete_check', [PrediosController::class, 'autocomplete_check']);
 Route::post('/get_predio', [PrediosController::class, 'get_predio']);
+Route::post('/avaluos_predio', [PrediosController::class, 'avaluos_predio']);
+Route::post('/get_propietario_by_identificacion', [PrediosController::class, 'get_propietario_by_identificacion']);
 
 //datos_basicos
 Route::post('/store/predios_datos_basicos', [PrediosController::class, 'store_predios_datos_basicos']);
 //datos_propietarios
 Route::post('/store/predios_datos_propietarios', [PrediosController::class, 'store_predios_datos_propietarios']);
 Route::post('/store/predios_propietarios_jerarquia', [PrediosController::class, 'store_predios_propietarios_jerarquia']);
+Route::post('/store/predios_propietarios_delete', [PrediosController::class, 'store_predios_propietarios_delete']);
 //datos_calculo
 Route::post('/store/predios_datos_calculo', [PrediosController::class, 'store_predios_datos_calculo']);
 //datos_pagos
@@ -243,7 +158,6 @@ Route::post('create_exoneraciones', [PrediosExoneracionesController::class, 'sto
 Route::post('update_exoneraciones', [PrediosExoneracionesController::class, 'update'])->name('prediosexoneraciones.update_exoneraciones')->middleware('checkdb');
 Route::post('delete_exoneraciones', [PrediosExoneracionesController::class, 'destroy'])->name('prediosexoneraciones.delete_exoneraciones')->middleware('checkdb');
 
-
 //TarifasPredialController
 Route::get('registro_tarifaspredial/{id}', [TarifasPredialController::class, 'create'])->middleware('checkdb');
 Route::get('registro_tarifaspredial', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
@@ -251,6 +165,14 @@ Route::get('registro_tarifaspredial', [ErrorRequestController::class, 'error_req
 Route::post('create_tarifaspredial', [TarifasPredialController::class, 'store'])->name('tarifaspredial.create_tarifaspredial')->middleware('checkdb');
 Route::post('update_tarifaspredial', [TarifasPredialController::class, 'update'])->name('tarifaspredial.update_tarifaspredial')->middleware('checkdb');
 Route::post('delete_tarifaspredial', [TarifasPredialController::class, 'destroy'])->name('tarifaspredial.delete_tarifaspredial')->middleware('checkdb');
+
+//TasasInteresController
+Route::get('registro_tasasinteres/{id}', [TasasInteresController::class, 'create'])->middleware('checkdb');
+Route::get('registro_tasasinteres', [ErrorRequestController::class, 'error_request'])->middleware('checkdb');
+
+Route::post('create_tasasinteres', [TasasInteresController::class, 'store'])->name('tasasinteres.create_tasasinteres')->middleware('checkdb');
+Route::post('update_tasasinteres', [TasasInteresController::class, 'update'])->name('tasasinteres.update_tasasinteres')->middleware('checkdb');
+Route::post('delete_tasasinteres', [TasasInteresController::class, 'destroy'])->name('tasasinteres.delete_tasasinteres')->middleware('checkdb');
 
 //TiposPredioController
 Route::get('registro_tipospredio/{id}', [TiposPredioController::class, 'create'])->middleware('checkdb');
@@ -268,4 +190,5 @@ Route::post('create_resolucion_igac', [ResolucionesIgacController::class, 'store
 Route::post('update_resolucion_igac', [ResolucionesIgacController::class, 'update'])->name('resoluciones_igac.update_resolucion_igac')->middleware('checkdb');
 Route::post('delete_resolucion_igac', [ResolucionesIgacController::class, 'destroy'])->name('resoluciones_igac.delete_resolucion_igac')->middleware('checkdb');
 
+//UploadController
 Route::post('uploadFileAsobancaria', [UploadController::class, 'uploadFileAsobancaria'])->name('upload-file-asobancaria');
