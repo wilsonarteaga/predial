6d49777aadf12f528727892c798b85c02e2d7836
Wilson Arteaga <wilson.arteaga@gmail.com>
2024-01-29 16:17:26 -0500

    Update PrediosController.php

--- app/Http/Controllers/PrediosController.php
+++ app/Http/Controllers/PrediosController.php
@@ -1499,8 +1499,6 @@ class PrediosController extends Controller
                 $barras_texto[$x] = ('(415)' . $ean . '(8020)' . str_pad($numero_factura , 24, "0", STR_PAD_LEFT) . '(3900)' . str_pad($valores_factura[$x], 14, "0", STR_PAD_LEFT) . '(96)' . str_replace('-', '', $fechas_pago_hasta[$x]));
             }
 
-            dd($predio);
-
             $data = [
                 'title' => 'Predio',
                 'fecha' => $ultimo_anio_pagar->primer_fecha != null ? $fecha_emision->format('d/m/Y') : 'INDEFINIDA',
